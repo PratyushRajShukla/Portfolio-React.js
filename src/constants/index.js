060e3563fb99cd0a3308ddf24c745a102e2c8749
PratyushRajShukla <pratyushraj.2089@gmail.com>
2023-09-20 21:29:10 +0530

    Added and Optimized About Section

--- src/constants/index.js
+++ src/constants/index.js
@@ -42,11 +42,11 @@ import {
   
   const services = [
     {
-      title: "Web Developer",
+      title: "Full-Stack Developer",
       icon: web,
     },
     {
-      title: "React Native Developer",
+      title: "Game Developer",
       icon: mobile,
     },
     {
@@ -54,7 +54,7 @@ import {
       icon: backend,
     },
     {
-      title: "Content Creator",
+      title: "React Dev",
       icon: creator,
     },
   ];
